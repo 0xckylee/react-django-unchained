30c5f61e9070658fa31eb5c8580f232ed82c707c
0xckylee <0xckylee@daum.net>
2020-03-10 18:42:46 +0900

    backend python virtualenv created for windows (venv-windows)

---
+++ backend/venv-windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\msecure\react-django-unchained\backend\venv-windows\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
